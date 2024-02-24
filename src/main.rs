f0c38a93336006ff95b28f50a3194ce40e44fc5e
katlyn-thomas <katlyn.c.thomas@gmail.com>
2024-02-24 16:47:55 -0600

    basic MVU arch

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,115 @@
-use crossterm::{
-    event::{self, KeyCode, KeyEventKind},
-    terminal::{
-        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
-        LeaveAlternateScreen,
-    },
-    ExecutableCommand,
-};
-use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
-};
-use std::io::{stdout, stderr};
-use anyhow::Result;
+use crossterm::event;
+use anyhow::{anyhow, bail, Result};
+use ratatui::{widgets::Paragraph, Frame};
 
-fn main() -> Result<()> {
-    stdout().execute(EnterAlternateScreen)?;
-    let _ = enable_raw_mode();
-    crossterm::execute!(stderr(), EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stderr()))?;
-    terminal.clear()?;
+#[derive(Debug, Default)]
+struct Model {
+    counter: isize,
+    app_state: AppState
+}
 
-    let mut counter = 0;
+#[derive(Debug, Default, PartialEq, Eq)]
+enum AppState {
+    #[default]
+    Running,
+    Stop
+}
 
-    loop {
-        let _ = terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new(format!("Counter {}", counter))
-                    .black()
-                    .on_blue(),
-                area
-            );
-        })?;
+enum Msg {
+    Increment,
+    Decrement,
+    Reset,
+    Quit
+}
 
-        if event::poll(std::time::Duration::from_millis(4))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    match key.code {
-                        KeyCode::Char('j') => counter += 1,
-                        KeyCode::Char('k') => counter -= 1,
-                        KeyCode::Char('q') => break,
-                        _ => {}
-                    }
+fn update(mut model: Model, msg: Msg) -> Model{
+    match msg {
+        Msg::Increment => model.counter += 1,
+        Msg::Decrement => model.counter -= 1,
+        Msg::Reset => model.counter = 0,
+        Msg::Quit => model.app_state = AppState::Stop,
+    }
+    model
+}
+
+fn view(model: &Model, frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new(format!("Counter: {}", model.counter)),
+        frame.size(),
+    )
+}
+
+fn handle_events() -> Result<Msg> {
+    if event::poll(std::time::Duration::from_millis(4))? {
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                return match key.code {
+                    event::KeyCode::Char('j') => Ok(Msg::Increment),
+                    event::KeyCode::Char('k') => Ok(Msg::Decrement),
+                    event::KeyCode::Char('r') => Ok(Msg::Reset),
+                    event::KeyCode::Char('q') => Ok(Msg::Quit),
+                    _ => bail!("no matching key")
                 }
             }
         }
     }
-    let _ = stdout().execute(LeaveAlternateScreen);
-    disable_raw_mode()?;
-    Ok(())
+    Err(anyhow!("no event"))
+}
+
+fn main() -> Result<()> {
+    tui::install_panic_hook();
+
+    let mut terminal = tui::init_app()?;
+
+    let mut model = Model::default();
+
+    while model.app_state == AppState::Running {
+        terminal.draw(|f| view(&model, f))?;
+        
+        let msg = handle_events();
+
+        if let Ok(msg) = msg {
+            model = update(model, msg);
+        }
+    }
+
+    tui::teardown_app()
+}
+
+mod tui {
+    use crossterm::{
+        terminal::{
+            disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+            LeaveAlternateScreen,
+        },
+        ExecutableCommand,
+    };
+    use ratatui::{
+        backend::Backend, prelude::{CrosstermBackend, Terminal},
+    };
+    use std::{io::stdout, panic};
+    use anyhow::{Ok, Result};
+
+    pub fn init_app() -> Result<Terminal<impl Backend>> {
+        stdout().execute(EnterAlternateScreen)?;
+        enable_raw_mode()?;
+        let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+        terminal.clear()?;
+        Ok(terminal)
+    }
+
+    pub fn teardown_app() -> Result<()> {
+        stdout().execute(LeaveAlternateScreen)?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn install_panic_hook() {
+        let original_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic_info| {
+            stdout().execute(LeaveAlternateScreen).unwrap();
+            disable_raw_mode().unwrap();
+            original_hook(panic_info);
+        }));
+    }
 }
